aa4288a714c8383fd4676ab8a0dd181548b5e6b4
GiovanniPalladino <giovanni.palladino@mail.polimi.it>
2025-12-13 12:36:50 +0100

    menù modified

--- p5js/menuVersion2.js
+++ p5js/menuVersion2.js
@@ -10,21 +10,19 @@ let menuSketch = function (p) {
   let menuOpen = false;
   let menuX;
   let menuTargetX;
-  let menuW = 250;
+  let menuW = 220;
 
   let font;
 
-  // link menu
   const menuLinks = {
-    "home": "index.html",
+    "NUCLEAR": "index.html",
     "overview": "index.html#page2",
     "bombs per year": "year.html",
     "insight": "insight.html",
     "about": "about.html"
   };
 
-  // ora include home
-  const items = ["home", "overview", "bombs per year", "insight", "about"];
+  const items = ["overview", "bombs per year", "insight", "about"];
 
   p.preload = function () {
     font = p.loadFont("fonts/LibreFranklin-Regular.otf");
@@ -51,6 +49,7 @@ let menuSketch = function (p) {
 
     drawSideMenu();
     drawButton();
+    drawTitleOutside();
   };
 
   function checkMenuLogic() {
@@ -62,6 +61,7 @@ let menuSketch = function (p) {
       btnY + btnSize / 2
     );
 
+    // hover bott
     hoverButton = distToButton < btnSize / 2 || menuOpen;
 
     if (distToButton < btnSize / 2) {
@@ -79,46 +79,57 @@ let menuSketch = function (p) {
     }
   }
 
-  // ---- BOTTONE ----
+  function drawTitleOutside() {
+    p.textFont(font);
+    p.textSize(14);
+
+    let title = "NUCLEAR EXPLOSIONS ARCHIVE";
+    let x = btnX + btnSize;
+    let y = btnY + 35;
+
+    let hover =
+      p.mouseX >= x &&
+      p.mouseX <= x + p.textWidth(title) &&
+      p.mouseY >= y - 5 &&
+      p.mouseY <= y + 20;
+
+    p.fill(hover ? p.color(0, 255, 255) : 255);
+    p.text(title, x, y);
+  }
+
+
+  // bottone
   function drawButton() {
 
-    let targetScale = hoverButton ? 1.65 : 1.5;
+    let targetScale = hoverButton ? 1.15 : 1.0;
     btnScale = p.lerp(btnScale, targetScale, 0.15);
 
     p.push();
     p.translate(btnX + btnSize / 2, btnY + btnSize / 2);
     p.scale(btnScale);
 
-    // sfondo bianco trasparente
-    p.noStroke();
-    p.fill(hoverButton ? p.color(0, 255, 255, 0) : p.color(255, 255, 255, 10));
-    p.ellipse(0, 0, 45, 45);
-
-    // cerchi pulsante
-    p.stroke(hoverButton ? p.color(0, 255, 255) : 255);
-    p.strokeWeight(0.4);
+    p.stroke(255);
+    p.strokeWeight(1);
     p.noFill();
-    p.ellipse(0, 0, 30, 10);
+     p.ellipse(0, 0, 30, 10);
 
-    p.push();
-    p.rotate(p.radians(60));
-    p.ellipse(0, 0, 30, 10);
-    p.pop();
+  p.push();
+  p.rotate(p.radians(60));  
+  p.ellipse(0, 0, 30, 10);
+  p.pop();
 
-    p.push();
-    p.rotate(p.radians(-60));
-    p.ellipse(0, 0, 30, 10);
-    p.pop();
+  p.push();
+  p.rotate(p.radians(-60)); 
+  p.ellipse(0, 0, 30, 10);
+  p.pop();
 
-    // punto centrale
-    p.fill(hoverButton ? p.color(0, 255, 255) : 255);
+    p.fill(255);
     p.noStroke();
-    p.ellipse(0, 0, 4);
+    p.ellipse(0, 0, 5);
 
     p.pop();
   }
 
-  // ---- MENU LATERALE ----
   function drawSideMenu() {
 
     if (!menuOpen && menuX <= -menuW + 1) return;
@@ -134,12 +145,11 @@ let menuSketch = function (p) {
 
       let label = items[i];
       let y = 120 + i * 28;
+
+      // voci menu
       let x = menuX + 38;
 
-      let displayLabel =
-        label === "home" ? "NUCLEAR EXPLOSIONS ARCHIVE" : label;
-
-      let w = p.textWidth(displayLabel);
+      let w = p.textWidth(label);
       let h = 18;
 
       let hovering =
@@ -149,11 +159,26 @@ let menuSketch = function (p) {
         p.mouseY <= y + h;
 
       p.fill(hovering ? p.color(0, 255, 255) : 220);
-      p.text(displayLabel, x, y);
+
+      p.text(label, x, y);
     }
   }
 
   p.mouseReleased = function () {
+
+    let title = "NUCLEAR EXPLOSIONS ARCHIVE";
+    let x = btnX + btnSize;
+    let y = btnY + 35;
+
+    if (
+      p.mouseX >= x &&
+      p.mouseX <= x + p.textWidth(title) &&
+      p.mouseY >= y - 5 &&
+      p.mouseY <= y + 20
+    ) {
+      window.location.href = "index.html";
+      return;
+    }
 
     if (!menuOpen) return;
 
@@ -162,11 +187,7 @@ let menuSketch = function (p) {
       let label = items[i];
       let y = 120 + i * 28;
       let x = menuX + 38;
-
-      let displayLabel =
-        label === "home" ? "NUCLEAR EXPLOSIONS ARCHIVE" : label;
-
-      let w = p.textWidth(displayLabel);
+      let w = p.textWidth(label);
       let h = 18;
 
       let hovering =
